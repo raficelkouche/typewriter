96cead443f72753a0fc93fe414e3c771332870a9
Rafic El Kouche <raficelkouche@hotmail.com>
2020-12-01 20:22:57 -0500

    added typewriter js that outputs characters of a sentence with a delay

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,13 @@
+const sentence = "hello there from lighthouse labs";
 
+let ms = 0;
+for (let i in sentence) {
+  setTimeout(() => {
+    process.stdout.write(sentence[i]);
+    if ( i == sentence.length - 1) {
+      console.log();
+    }
+  }, ms)
+  
+  ms+=50;
+}
